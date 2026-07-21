a7b93c5dcf0ddd98e31da19e629e8573788e2870
victor-martinez87 <martinez.victor87@gmail.com>
2026-07-21 19:37:53 +0200

    routes(api): register battle and user-pokemon API endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Api\BattleController;
+use App\Http\Controllers\Api\UserPokemonController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/battle/calculate', [BattleController::class, 'calculate']);
+
+Route::apiResource('user-pokemons', UserPokemonController::class);
